5eadd52d6436b6bee6797d49c933c4e12f9271ba
Steve-win11-main <liunandri@gmail.com>
2025-07-04 02:40:57 +0200

    update//customize//complete configuration

--- Generator/Generate.cs
+++ Generator/Generate.cs
@@ -7,65 +7,150 @@ namespace Schneegans.Unattend;
 
 class Generate
 {
-  public static void Main(string[] args){
-    UnattendGenerator generator = new();
+    public static void Main(string[] args)
+    {
+        var outputDir = EnsureOutputDirectory();
+        var generator = new UnattendGenerator();
 
-    // checkout https://github.com/kaliiiiiiiiii/unattend-generator/blob/master/Main.cs
-    XmlDocument xml = generator.GenerateXml(
-      Configuration.Default with
-      {
-        LanguageSettings = new UnattendedLanguageSettings(
-          ImageLanguage: generator.Lookup<ImageLanguage>("en-US"),
-          LocaleAndKeyboard: new LocaleAndKeyboard(
-            generator.Lookup<UserLocale>("en-US"),
-            generator.Lookup<KeyboardIdentifier>("00060409") // colemak
-          ),
-          LocaleAndKeyboard2: null,
-          LocaleAndKeyboard3: null,
-          GeoLocation: generator.Lookup<GeoLocation>("223") // switzerland
-        ),
-        Bloatwares = ImmutableList.CreateRange(
-          [
-            generator.Lookup<Bloatware>("RemoveCopilot"),
-          ]
-        ),
-        BypassRequirementsCheck = true,
-        ShowFileExtensions = true,
-        EnableLongPaths = true,
-        HideEdgeFre = true,
-        DeleteWindowsOld = true,
-        DisableBingResults = true,
-        StickyKeysSettings = new DisabledStickyKeysSettings(),
-        ColorSettings = new CustomColorSettings(
-          SystemTheme: ColorTheme.Dark,
-          AppsTheme: ColorTheme.Dark,
-          EnableTransparency: false,
-          AccentColorOnStart: false,
-          AccentColorOnBorders: false,
-          AccentColor: Color.FromArgb(0, 120, 215)
-        )
-      }
-    );
-
-    string outputDir = Path.Combine(Environment.CurrentDirectory, "out");
-    if (!Directory.Exists(outputDir)){
-      Directory.CreateDirectory(outputDir);
+        var xml = generator.GenerateXml(GenerateSettings(generator));
+        var xmlPath = WriteXmlFile(xml, outputDir);
+        CreateIso(outputDir, xmlPath);
     }
-    string path = Path.Combine(outputDir, "autounattend.xml");
-    File.WriteAllBytes(path, UnattendGenerator.Serialize(xml));
-    string isoPath = Path.Combine(outputDir, "devwin.iso");
 
-    using (FileStream isoStream = File.Create(isoPath)) {
+    static string EnsureOutputDirectory()
+    {
+        string outputDir = Path.Combine(Environment.CurrentDirectory, "out");
+        if (!Directory.Exists(outputDir))
+        {
+            Directory.CreateDirectory(outputDir);
+        }
+        return outputDir;
+    }
+
+    static Configuration GenerateSettings(UnattendGenerator generator)
+    {
+        return Configuration.Default with
+        {
+            LanguageSettings = new UnattendedLanguageSettings(
+                generator.Lookup<ImageLanguage>("en-US"),
+                new LocaleAndKeyboard(
+                    generator.Lookup<UserLocale>("en-US"),
+                    generator.Lookup<KeyboardIdentifier>("00060409")
+                ),
+                null, null,
+                generator.Lookup<GeoLocation>("223")
+            ),
+            AccountSettings = new InteractiveLocalAccountSettings(),
+            PartitionSettings = new InteractivePartitionSettings(),
+            InstallFromSettings = new AutomaticInstallFromSettings(),
+            DiskAssertionSettings = new SkipDiskAssertionSettings(),
+            EditionSettings = new CustomEditionSettings(productKey: "VK7JG-NPHTM-C97JM-9MPGT-3V66T"),
+            LockoutSettings = new DefaultLockoutSettings(),
+            PasswordExpirationSettings = new UnlimitedPasswordExpirationSettings(),
+            ProcessAuditSettings = new DisabledProcessAuditSettings(),
+            ComputerNameSettings = new RandomComputerNameSettings(),
+            TimeZoneSettings = new ExplicitTimeZoneSettings(
+                new TimeOffset("W. Europe Standard Time", "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna")
+            ),
+            WifiSettings = new InteractiveWifiSettings(),
+            WdacSettings = new SkipWdacSettings(),
+            ProcessorArchitectures = [ProcessorArchitecture.amd64],
+            Components = ImmutableDictionary.Create<ComponentAndPass, string>(),
+            Bloatwares = ImmutableList.CreateRange([
+                generator.Lookup<Bloatware>("RemoveCopilot"),
+                generator.Lookup<Bloatware>("RemoveOneDrive"),
+            ]),
+            ExpressSettings = ExpressSettingsMode.DisableAll,
+            ScriptSettings = new ScriptSettings(Scripts: [], RestartExplorer: false),
+            KeySettings = new SkipKeySettings(),
+            WallpaperSettings = new SolidWallpaperSettings(Color.Black),
+            ColorSettings = new CustomColorSettings(
+                ColorTheme.Dark, ColorTheme.Dark, false, false, false, Color.FromArgb(0, 120, 215)
+            ),
+            BypassRequirementsCheck = true,
+            BypassNetworkCheck = true,
+            EnableLongPaths = true,
+            EnableRemoteDesktop = false,
+            HardenSystemDriveAcl = true,
+            AllowPowerShellScripts = false,
+            DisableLastAccess = true,
+            PreventAutomaticReboot = true,
+            DisableDefender = false,
+            DisableSac = false,
+            DisableUac = false,
+            DisableSmartScreen = false,
+            DisableFastStartup = false,
+            DisableSystemRestore = false,
+            TurnOffSystemSounds = true,
+            DisableAppSuggestions = true,
+            DisableWidgets = true,
+            VBoxGuestAdditions = false,
+            VMwareTools = false,
+            VirtIoGuestTools = false,
+            PreventDeviceEncryption = false,
+            ClassicContextMenu = true,
+            LeftTaskbar = false,
+            HideTaskViewButton = false,
+            ShowFileExtensions = true,
+            ShowAllTrayIcons = false,
+            HideFiles = HideModes.None,
+            HideEdgeFre = true,
+            DisableEdgeStartupBoost = true,
+            MakeEdgeUninstallable = true,
+            LaunchToThisPC = true,
+            DisableWindowsUpdate = false,
+            DisablePointerPrecision = true,
+            DeleteWindowsOld = true,
+            DisableBingResults = true,
+            UseConfigurationSet = false,
+            HidePowerShellWindows = false,
+            ShowEndTask = true,
+            TaskbarSearch = TaskbarSearchMode.Hide,
+            StartPinsSettings = new CustomStartPinsSettings(
+                Json: "{\"pinnedList\":[{\"desktopAppLink\":\"%APPDATA%\\\\Microsoft\\\\Windows\\\\Start Menu\\\\Programs\\\\File Explorer.lnk\"}]}"
+            ),
+            StartTilesSettings = new EmptyStartTilesSettings(),
+            StickyKeysSettings = new DisabledStickyKeysSettings(),
+            CompactOsMode = CompactOsModes.Default,
+            TaskbarIcons = new CustomTaskbarIcons(Xml: @"<?xml version=""1.0"" encoding=""utf-8""?>
+<LayoutModificationTemplate
+    xmlns=""http://schemas.microsoft.com/Start/2014/LayoutModification""
+    xmlns:defaultlayout=""http://schemas.microsoft.com/Start/2014/FullDefaultLayout""
+    xmlns:start=""http://schemas.microsoft.com/Start/2014/StartLayout""
+    xmlns:taskbar=""http://schemas.microsoft.com/Start/2014/TaskbarLayout""
+    Version=""1"">
+  <CustomTaskbarLayoutCollection PinListPlacement=""Replace"">
+    <defaultlayout:TaskbarLayout>
+      <taskbar:TaskbarPinList>
+        <taskbar:DesktopApp DesktopApplicationID=""Microsoft.Windows.Explorer""/>
+      </taskbar:TaskbarPinList>
+    </defaultlayout:TaskbarLayout>
+  </CustomTaskbarLayoutCollection>
+</LayoutModificationTemplate>"),
+            Effects = new DefaultEffects(),
+            DesktopIcons = new DefaultDesktopIconSettings(),
+        };
+    }
+
+    static string WriteXmlFile(XmlDocument xml, string outputDir)
+    {
+        string path = Path.Combine(outputDir, "autounattend.xml");
+        File.WriteAllBytes(path, UnattendGenerator.Serialize(xml));
+        return path;
+    }
+
+    static void CreateIso(string outputDir, string xmlPath)
+    {
+        string isoPath = Path.Combine(outputDir, "devwin.iso");
+
+        using FileStream isoStream = File.Create(isoPath);
         CDBuilder builder = new CDBuilder
         {
             UseJoliet = true,
             VolumeIdentifier = "DEVWIN"
         };
 
-        // Add autounattend.xml to the root of the ISO
-        builder.AddFile("autounattend.xml", path);
-
+        builder.AddFile("autounattend.xml", xmlPath);
         builder.Build(isoStream);
     }
-  }
 }
