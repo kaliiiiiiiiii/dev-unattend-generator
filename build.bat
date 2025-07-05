de41bcc3981e88943333b3c8a227f660fb0091a0
Steve-win11-main <liunandri@gmail.com>
2025-07-05 02:28:23 +0200

    fix build.bat exit code

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
-robocopy "./defaultconfig" "./config" /E /PURGE
-robocopy "./defaultconfig" "./out/bin/config" /E /PURGE
+robocopy "./defaultconfig" "./config" /E /PURGE /NJH /NJS
+IF %ERRORLEVEL% GTR 2 EXIT /B %ERRORLEVEL%
+
+robocopy "./defaultconfig" "./out/bin/config" /E /PURGE /NJH /NJS
+IF %ERRORLEVEL% GTR 2 EXIT /B %ERRORLEVEL%
+
 dotnet build -c Release "./Generator/generate.csproj"
-robocopy "./Generator/bin/Release/net9.0" "./out/bin" /E
+
+robocopy "./Generator/bin/Release/net9.0" "./out/bin" /E /NJH /NJS /XD "config"
+IF %ERRORLEVEL% GTR 2 EXIT /B %ERRORLEVEL%
